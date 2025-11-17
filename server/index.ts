e203053b91a4f1d4b470df140fcdae2bdbcf1fbb
shub-10 <116427176+shub-10@users.noreply.github.com>
2025-11-17 19:47:49 +0530

    initial setup

---
+++ server/index.ts
@@ -0,0 +1,40 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import http from 'http';
+import {Server} from 'socket.io';
+import {createServer} from "http";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors:{
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+app.use(express.json());
+app.use(cors());
+const port = process.env.port || 3000;
+
+
+io.on('connection', (socket)=>{
+  console.log(`new user connected with id ${socket.id}`);
+
+  socket.on("join-board", (boardId)=>{
+    socket.join(boardId);
+    console.log(`user ${socket.id} joined board: ${boardId}`)
+  });
+  socket.on("draw", data=>{
+    socket.to(data.boardId).emit("draw", data);
+  });
+
+  socket.on("disconnect", ()=>{
+    console.log("user disconnected: ", socket.id);
+  });
+})
+app.get('/', (req: Request, res:Response)=>{
+  res.json("server is running");
+})
+server.listen(port, ()=>{
+  console.log(`server running at 🚀: ${port}`)
+})
